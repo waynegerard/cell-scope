d1d1b62fef26a43a0d469f488a96dbc2bea13560
waynegerard <WayneGerard@gmail.com>
2012-12-14 00:51:39 -0500

    Removing unneeded files, stubbing out hu moment calculator

--- CellScope/matlab/objlevel/FeatureCalculator.m
+++ CellScope/matlab/objlevel/FeatureCalculator.m
@@ -10,11 +10,61 @@
 
 @implementation FeatureCalculator
 
+// NOTES: This should be refactored into one method both of the two below functions call
+
+- (void) showGrayPatches {
+    int j = 0;
+    NSMutableArray* allPatches = [NSMutableArray array];
+    int numPatchesPerCol = 10;
+    int extra = numPatchesPerCol - (blobs.numObjects %% numPatchesPerCol);
+    for (int n = 0; n < blobs.numObjects + extra; n++) {
+        j++;
+        if (n > blobs.numObjects) {
+            allPatches[1][j] = makeZeroMatrix(sz, sz);
+        } else {
+            patch = blobs.stats(n).patch;
+            if patch.isEmpty() {
+                allPatches[1][j] = makeZeroMatrix(sz, sz);
+            } else {
+                allPatches[1][j] = patch;
+            }
+        }
+    }
+    allPatches = reshape(allPatches, numPatchesPerCol, []);
+    imAllPatches = cell2mat(allPatches);
+}
+
+- (void) showBinPatches {
+    int j = 0;
+    NSMutableArray* allPatches = [NSMutableArray array];
+    int numPatchesPerCol = 10;
+    int extra = numPatchesPerCol - (blobs.numObjects %% numPatchesPerCol);
+    for (int n = 0; n < blobs.numObjects + extra; n++) {
+        j++;
+        if n > blobs.numObjects {
+            allPatches[1][j] = logical(ones(sz, sz)); // TODO: Figure out a C++ way to do this
+        }
+        else {
+            patch = blobs.stats(n).binpatch;
+            if isEmpty(patch) {
+                allPatches[1][j] = logical(ones(sz, sz)); // TODO: Figure out a C++ way to do this
+            } else {
+                allPatches[1][j] = patch;
+            }
+        }
+    }
+    
+    allPatches = reshape(allPatches, numPatchesPerCol, []);
+    imAllPatches = cell2mat(allPatches);
+    
+}
+
 /**
     Takes the patches and calculates various Hu moments, geometric, and photometric features
     @param blobs 
  */
-- (void) calculateFeaturesWithBlobs: (NSObject*) blobs withPatchSize:(int) patchSize doHoG:(BOOL) HoG {
+- (void) calculateFeaturesWithBlobs: (NSObject*) blobs withPatchSize:(int) patchSize doHoG:(BOOL) HoG showGrayPatches: (BOOL) showGrayPatches showBinPatches:(BOOL) showBinPatches  {
+    
     for (int i = 0; i < blobs.numObjects; i++) {
         patch = blobs.stats(n).patch;
         
@@ -30,59 +80,14 @@
     }
     
     // Visually check binarization of patches
-    BOOL showGrayPatches = NO;
-    BOOL showBinPatches = NO;
     int sz = patchSize;
     
     if (showGrayPatches) {
-        int j = 0;
-        NSMutableArray* allPatches = [NSMutableArray array];
-        int numPatchesPerCol = 10;
-        int extra = numPatchesPerCol - (blobs.numObjects %% numPatchesPerCol);
-        for (int n = 0; n < blobs.numObjects + extra; n++) {
-            j++;
-            if (n > blobs.numObjects) {
-                allPatches[1][j] = makeZeroMatrix(sz, sz);
-            } else {
-                patch = blobs.stats(n).patch;
-                if patch.isEmpty() {
-                    allPatches[1][j] = makeZeroMatrix(sz, sz);
-                } else {
-                    allPatches[1][j] = patch;
-                }
-            }
-        }
-        allPatches = reshape(allPatches, numPatchesPerCol, []);
-        imAllPatches = cell2mat(allPatches);
-        // TODO: do we need this?
-        // figure imshow(imallpatches)l title (grayscale patches in calcfeats)        
+        [self showGrayPatches];
     }
     
     if (showBinPatches) {
-        int j = 0;
-        NSMutableArray* allPatches = [NSMutableArray array];
-        int numPatchesPerCol = 10;
-        int extra = numPatchesPerCol - (blobs.numObjects %% numPatchesPerCol);
-        for (int n = 0; n < blobs.numObjects + extra; n++) {
-            j++;
-            if n > blobs.numObjects {
-                allPatches[1][j] = logical(ones(sz, sz)); // TODO: Figure out a C++ way to do this
-            }
-            else {
-                patch = blobs.stats(n).binpatch;
-                if isEmpty(patch) {
-                    allPatches[1][j] = logical(ones(sz, sz)); // TODO: Figure out a C++ way to do this
-                } else {
-                    allPatches[1][j] = patch;
-                }
-            }
-        }
-        
-        allPatches = reshape(allPatches, numPatchesPerCol, []);
-        imAllPatches = cell2mat(allPatches);
-        
-        // TODO: do we need this?
-        // figure; imshow(imallpatches); title('Bin patches in calcfeats');
+        [self showBinPatches];
     }
     
 }
--- CellScope/matlab/objlevel/HuMomentCalculator.h
+++ CellScope/matlab/objlevel/HuMomentCalculator.h
@@ -1,13 +1,13 @@
 //
-//  Train_Algorithm.h
+//  HuMomentCalculator.h
 //  CellScope
 //
-//  Created by Wayne Gerard on 12/10/12.
+//  Created by Wayne Gerard on 12/13/12.
 //  Copyright (c) 2012 Matthew Bakalar. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface Train_Algorithm : NSObject
+@interface HuMomentCalculator : NSObject
 
 @end
